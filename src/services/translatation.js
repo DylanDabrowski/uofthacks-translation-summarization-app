864385d759ce28e093bf915f4bdc7f076e2f2b65
Dylan Dabrowski <dylandabrowski@gmail.com>
2023-01-21 17:06:41 -0500

    language detection into translation with selector to choose output language is working as intented

--- src/services/translatation.js
+++ src/services/translatation.js
@@ -1,26 +1,3 @@
-export const translateText = async (fromLang, toLang, text) => {
-  let url = `https://translation.googleapis.com/language/translate/v2?key=${
-    import.meta.env.VITE_TRANSLATION_KEY
-  }`;
-  url += "&q=" + encodeURI(text);
-  url += `&source=${fromLang}`;
-  url += `&target=${toLang}`;
-
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    });
-    const json = await response.json();
-    return json.data.translations[0].translatedText;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export const detectLanguage = async (text) => {
   let url = `https://translation.googleapis.com/language/translate/v2/detect?key=${
     import.meta.env.VITE_TRANSLATION_KEY
@@ -37,6 +14,33 @@ export const detectLanguage = async (text) => {
     });
     const json = await response.json();
     return json.data.detections[0][0].language;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const translateText = async (toLang, text) => {
+  const fromLang = await detectLanguage(text);
+
+  let url = `https://translation.googleapis.com/language/translate/v2?key=${
+    import.meta.env.VITE_TRANSLATION_KEY
+  }`;
+  url += "&q=" + encodeURI(text);
+  url += `&source=${fromLang}`;
+  url += `&target=${toLang}`;
+
+  console.log(fromLang, toLang, text);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const json = await response.json();
+    return json.data.translations[0].translatedText;
   } catch (error) {
     console.error(error);
   }
