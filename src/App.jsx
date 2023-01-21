4cc608e8d1a19a9193f42faf13013e020d202f05
Dylan Dabrowski <dylandabrowski@gmail.com>
2023-01-21 18:19:27 -0500

    camera working as a component that can open and close, not doing anything with the image it takes yet

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import languages from "./assets/langs";
+import CameraComponent from "./components/CameraComponent";
 
 function App() {
   const [text, setText] = useState("");
   const [translatedText, setTranslatedText] = useState("");
   const [desiredLanguage, setDesiredLanguage] = useState("en");
+  const [cameraOpen, setCameraOpen] = useState(false);
+  const [photo, setPhoto] = useState();
 
   const handleSubmit = async () => {
     const newtext = await translateText(desiredLanguage, text);
@@ -22,9 +25,32 @@ function App() {
     console.log(desiredLanguage);
   };
 
+  function handleSubmitPhoto(dataUri) {
+    // Do stuff with the photo...
+    console.log("submit photo");
+
+    setCameraOpen(false);
+  }
+
   return (
     <div className="App">
       <div>
+        <button
+          onClick={() => {
+            setCameraOpen(true);
+          }}
+        >
+          Open Camera
+        </button>
+        {cameraOpen ? (
+          <CameraComponent
+            handleTakePhoto={handleSubmitPhoto}
+            photo={photo}
+            setCameraOpen={setCameraOpen}
+          />
+        ) : (
+          <></>
+        )}
         <textarea value={text} onChange={(e) => setText(e.target.value)} />
         <FormControl fullWidth>
           <InputLabel id="select-label">Output</InputLabel>
